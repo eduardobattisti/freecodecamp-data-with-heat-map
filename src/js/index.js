75b1ae40cb36a07ef29b902773403e0c9562945b
eduardobattisti <eduardobattisti10@gmail.com>
2021-05-31 13:02:25 -0300

    creating th y and x axis. Gettin the data and treating

--- src/js/index.js
+++ src/js/index.js
@@ -4,30 +4,63 @@ import * as D3 from '../../node_modules/d3/dist/d3.js';
 
 	const subtitle = document.getElementById('subtitle');
 
+	const colors = {
+		1: '#DBE2AF',
+		2: '#EDD96E',
+		3: '#F3BF5E',
+		4: '#E89B53',
+		5: '#CF7047',
+		6: '#A93E3C',
+		7: '#7A002D',
+	}
+
+	const months = {
+		1: 'January',
+		2: 'February',
+		3: 'March',
+		4: 'April',
+		5: 'May',
+		6: 'June',
+		7: 'July',
+		8: 'August',
+		9: 'September',
+		10: 'October',
+		11: 'November',
+		12: 'December',
+	}
+
 	const dataset = await fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
 		.then((response) => response.json())
 		.then((data) => data)
 		.catch((err) => console.log(`Something happened: ${err}`));
 
 	console.log(dataset);
+	console.log(months);
 
 	const w = 1260;
 	const h = 500;
 	const padding = 20;
 	const monthlyVariance = dataset.monthlyVariance.map((elem) => elem);
-	subtitle.innerText = `1753 - 2015: Base Temperature $${dataset.baseTemperature}°C`
+	const years = Array.from(new Set(monthlyVariance.map(elem => elem.year)));
+	const numberMonths = Array.from(new Set(monthlyVariance.map(elem => elem.month)));
 
-	// const dates = dataset.data.map((elem) => new Date(elem[0]));
+	subtitle.innerText = `${d3.min(years)} - ${d3.max(years)}: Base Temperature $${dataset.baseTemperature}°C`
+
+	const dates = monthlyVariance.map((elem) => new Date(elem.year, (elem.month - 1)));
+	dates.map(date => {
+		return date.toLocaleString('EN', { month: 'long' });
+	})
 	// const maxDate = new Date(d3.max(dates));
-	// maxDate.setMonth(maxDate.getMonth() + 3);
 
-	// const xScale = d3.scaleTime()
-	// 	.domain([d3.min(dates), maxDate])
-	// 	.range([padding * 2.2, w - padding]);
+	console.log(dates);
 
-	// const yScale = d3.scaleLinear()
-	// 	.domain([0, d3.max(gdp)])
-	// 	.range([h - padding * 3, 15]);
+	const xScale = d3.scaleTime()
+		.domain([d3.min(dates), d3.max(dates)])
+		.range([padding * 2.2, w - padding]);
+
+	const yScale = d3.scaleTime()
+		.domain([d3.min(dates), d3.max(dates)])
+		.range([h - padding * 3, 15]);
 
 	// const linearScale = d3.scaleLinear()
 	// 	.domain([0, d3.max(gdp)])
@@ -36,12 +69,12 @@ import * as D3 from '../../node_modules/d3/dist/d3.js';
 	// const valueScaled = gdp.map((elem) => linearScale(elem));
 
 	// const xAxis = d3.axisBottom(xScale);
-	// const yAxis = d3.axisLeft(yScale);
+	const yAxis = d3.axisLeft(yScale);
 
-	// const svg = d3.select('#wrapper-bar-chart')
-	// 	.append('svg')
-	// 	.attr('width', w)
-	// 	.attr('height', h);
+	const svg = d3.select('#wrapper-bar-chart')
+		.append('svg')
+		.attr('width', w)
+		.attr('height', h);
 
 	// svg.append('text')
 	// 	.attr('transform', 'rotate(-90)')
@@ -55,20 +88,18 @@ import * as D3 from '../../node_modules/d3/dist/d3.js';
 	// 	.text('More Information: http://www.bea.gov/national/pdf/nipaguid.pdf')
 	// 	.attr('class', 'info');
 
-	// const barChart = d3.select('svg')
-	// 	.selectAll('rect')
-	// 	.data(valueScaled)
-	// 	.enter()
-	// 	.append('rect')
-	// 	.attr('x', (d, i) => (i * 3.04) + padding * 2.2)
-	// 	.attr('y', (d, i) => h - d - (padding * 3))
-	// 	.attr('data-date', (d, i) => dataset.data[i][0])
-	// 	.attr('data-gdp', (d, i) => dataset.data[i][1])
-	// 	.attr('width', '2')
-	// 	.attr('height', (d) => d)
-	// 	.attr('fill', '#242440')
-	// 	.attr('class', 'bar')
-	// 	.append('title');
+	const barChart = d3.select('svg')
+		.selectAll('rect')
+		.data(monthlyVariance.map(elem => dataset.baseTemperature + elem.variance))
+		.enter()
+		.append('rect')
+		.attr('x', (d, i) => (i * 3.04) + padding * 2.2)
+		.attr('y', (d, i) => h - d - (padding * 3))
+		.attr('width', '2')
+		.attr('height', (d) => d)
+		.attr('fill', '#242440')
+		.attr('class', 'bar')
+		.append('title');
 
 	// svg.append('g')
 	// 	.attr('id', 'x-axis')
@@ -76,11 +107,11 @@ import * as D3 from '../../node_modules/d3/dist/d3.js';
 	// 	.call(xAxis)
 	// 	.attr('fill', 'none');
 
-	// svg.append('g')
-	// 	.attr('id', 'y-axis')
-	// 	.attr('transform', `translate(${padding * 2.2}, 0)`)
-	// 	.call(yAxis)
-	// 	.attr('fill', 'none');
+	svg.append('g')
+		.attr('id', 'y-axis')
+		.attr('transform', `translate(${padding * 2.2}, 0)`)
+		.call(yAxis)
+		.attr('fill', 'none');
 
 	// const bars = document.getElementsByClassName('bar');
 	// for (const bar of bars) {
